2a7986ad2c505472ecfa1f473ac284797dc2fa6f
Yusuke Uehara <uskey1024@gmail.com>
2024-10-12 20:15:52 +0900

    add Spring Initializr generate files

---
+++ src/main/java/com/uskey512/csp_sample/CspSampleApplication.java
@@ -0,0 +1,13 @@
+package com.uskey512.csp_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CspSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CspSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uskey512/csp_sample/CspSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uskey512.csp_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CspSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
